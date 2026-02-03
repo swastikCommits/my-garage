b11fdd6f413d2074ec30ec4f52717eb526e0cb12
swastikCommits <textswastik.alt@gmail.com>
2026-02-03 09:44:20 +0530

    init w tailwindcss

---
+++ onlyBucks/apps/user-app/app/page.tsx
@@ -0,0 +1,7 @@
+export default function App(){
+  return(
+    <div className="flex flex-col items-center justify-center h-screen"> 
+      <h1 className=" text-blue-500">User App Home Page</h1>
+    </div>  
+  )
+}
